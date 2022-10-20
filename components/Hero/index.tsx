9babb147bda0a1d0238035976011a5ae8215cce5
Piotr Pabich <ppabich@valuelogic.one>
2022-10-20 08:41:05 +0200

    Badges were not render correctly in mobile view

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -22,11 +22,17 @@ const HeroStyled = tw.div`
 
 const LayersStyled = tw.div`
   h-[520px]
-  [> div]:(absolute inset-0 flex items-center justify-center)
+  [> div]:(absolute flex items-center justify-center)
+  [> .full-cover]:(inset-0)
 `
 
 const GradientOverlay = tw.div`
   bg-gradient-to-b from-transparent via-transparent to-vlr-dark
+`
+
+const BadgeContainer = tw.div`
+  z-40 w-20 top-14 left-1/2 translate-x-[-50%] lg:left-28 lg:top-1/2 lg:translate-y-[-50%]
+  after:(z-[-1] absolute inset-0 bg-black/10 shadow-lg backdrop-blur-sm clip-path[polygon(0 0, 100% 0, 100% 80%, 50% 100%, 0 80%)])
 `
 
 const Hero = ({ id, title, badge: Badge, motive: Motive, background }: HeroProps) => {
@@ -34,17 +40,17 @@ const Hero = ({ id, title, badge: Badge, motive: Motive, background }: HeroProps
     <HeroStyled>
       <h2 tw="z-50 text-6xl font-weight[600] text-shadow[0 8px 15px rgb(0 0 0)]">{title}</h2>
       <LayersStyled>
-        <div tw="z-40 w-20 !left-28">
+        <BadgeContainer>
           <Badge />
-        </div>
-        <Link href={`/republic/${id}`}>
+        </BadgeContainer>
+        <Link className="full-cover" href={`/republic/${id}`}>
           <LinkButton tw="z-40 bottom-14 absolute left-[50%] translate-x-[-50%] py-2">Dowiedz się więcej</LinkButton>
         </Link>
-        <GradientOverlay tw="z-30" />
-        <div tw="z-20">
+        <GradientOverlay className="full-cover" tw="z-30" />
+        <div className="full-cover" tw="z-20 px-5 md:px-0">
           <Motive />
         </div>
-        <div tw="z-10">
+        <div className="full-cover" tw="z-10">
           <Image src={background} alt="background" layout="fill" objectFit="cover" objectPosition="center" />
         </div>
       </LayersStyled>
