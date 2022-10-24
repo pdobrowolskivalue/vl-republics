ec686fb989c236719503316579bd27125618d0b8
adrianczekaj <aczekaj.inf@gmail.com>
2022-10-24 15:14:23 +0200

    Create republic page - before code refactoring

--- components/common/LinkButton.tsx
+++ components/common/LinkButton.tsx
@@ -1,5 +1,9 @@
 import tw from 'twin.macro'
 
+export const SmallLinkButton = tw.a`
+  border rounded-3xl py-0.5 px-3 text-center cursor-pointer text-xs font-semibold
+`
+
 export const LinkButton = tw.a`
   border rounded-3xl py-3 px-7 text-center cursor-pointer
 `
