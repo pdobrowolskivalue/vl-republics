cd7b5c9987f8724254c733d21a797c306baab1bf
adrianczekaj <aczekaj.inf@gmail.com>
2022-10-26 15:04:23 +0200

    Set welcome cards as centered

--- components/Republic/WelcomeCard.tsx
+++ components/Republic/WelcomeCard.tsx
@@ -6,11 +6,10 @@ import { RepublicAboutUs } from '@components/big-icons/RepublicAboutUs'
 import { RepublicRulesTips } from '@components/big-icons/RepublicRulesTips'
 
 const Container = tw.div`
-  pt-12 pb-8 px-8 mr-10
+  pt-12 pb-8 px-8 mx-5
   bg-white rounded-md shadow-[0 1px 0 vlr-shadow]
   w-[290px] max-w-[290px]
   flex flex-col
-  gap-[27px]
 `
 
 const renderIcon = ({ iconUrl }: { iconUrl: string }) => {
