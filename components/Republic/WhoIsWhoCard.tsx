3ca4dd471899639e4792ff8dcae46636f8f51775
adrianczekaj <aczekaj.inf@gmail.com>
2022-11-02 10:19:00 +0100

    Cut photo to hexagonal shape

--- components/Republic/WhoIsWhoCard.tsx
+++ components/Republic/WhoIsWhoCard.tsx
@@ -1,9 +1,26 @@
 import 'twin.macro'
 
+const Photo = () => {
+  return (
+    <div tw="overflow-hidden invisible rotate-[120deg] w-[88px] h-[176px] m-[-37px 0 -37px 0]">
+      <div tw="overflow-hidden w-full h-full rotate-[-60deg]">
+        <div
+          tw="w-full h-full bg-no-repeat bg-cover visible rotate-[-60deg] bg-center"
+          style={{
+            backgroundImage: 'url(http://placekitten.com/240/240)',
+          }}
+        ></div>
+      </div>
+    </div>
+  )
+}
+
 export const WhoIsWhoCard = ({ role, name, description }: { role: string; name: string; description: string }) => {
   return (
     <div tw="flex py-6 px-[60px] items-center text-left border-solid border-[1px] border-vlr-accent/20 rounded-lg backdrop-blur-sm">
-      <div tw="mr-20 border-solid border-2 border-white/20 w-[88px] h-[88px] text-center" />
+      <div tw="mr-20">
+        <Photo />
+      </div>
       <div tw="mr-20 max-w-[205px] w-[205px]">
         <div tw="text-sm">{role}</div>
         <div tw="text-[22px] leading-[33px] font-medium">{name}</div>
